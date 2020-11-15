7bffae212222309b24bca3790f4817b239bcdb10
Ramadhan Adi P <ramadhanadiputra1@gmail.com>
2020-11-15 11:04:02 +0700

    okay

--- main.py
+++ main.py
@@ -38,27 +38,9 @@ driver.find_element_by_xpath('//*[@id="i267"]/div[3]/div').click()
 driver.find_element_by_xpath('//*[@id="i274"]/div[3]/div').click()
 driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div/div/span').click()
 
-
-
-"""
-def foo():
-    from selenium.webdriver.support.ui import WebDriverWait
-    from selenium.webdriver.common.by import By
-    from selenium.webdriver.support import expected_conditions as EC
-    driver = webdriver.Chrome(
-
-    executable_path=os.path.abspath('chromedriver'))  # Optional argument, if not specified will search path.
-    driver.get('https://docs.google.com/forms/d/e/1FAIpQLScbs4_3hPNYgjUO-hIa-H1OfJiDZ-FIY1WSk31jGyW5UtQ-Ow/viewform')
-    WebDriverWait(driver,20).until(EC.element_to_be_clickable((By.XPATH,"//div[@class='quantumWizMenuPaperselectOptionList']"))).click()
-    WebDriverWait(driver,20).until(EC.element_to_be_clickable((By.XPATH,"//div[@class='exportSelectPopup quantumWizMenuPaperselectPopup']//div[@class='quantumWizMenuPaperselectOption freebirdThemedSelectOptionDarkerDisabled exportOption' and @data-value='Option 2']//span[text()='Option 2']"))).click()
-
-foo()
-"""
 """
 from pyautogui import write
-
 option_number = 3 # Choose option number here
-
 # Put this where the program's supposed to select an option
 for _ in range(option_number):
     write(['down'])
